f769f8f0bf134c297d0c02165897c3c60a4f06a6
DuyAnh97xx <49968800+DuyAnh97xx@users.noreply.github.com>
2019-08-22 08:39:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/builddewarp/SpinnerItem.java
@@ -0,0 +1,13 @@
+package com.example.builddewarp;
+
+class SpinnerItem {
+    private String mItem;
+
+    SpinnerItem(String string){
+        mItem = string;
+    }
+
+    String getmLanguage() {
+        return mItem;
+    }
+}
